fd5424dcca9da8fd939ff4b403c024e98584eec0
Dhruven Shah <Dhruven_Shah@epam.com>
2020-07-26 22:36:14 -0700

    Adding pom.xml and SRC

---
+++ src/test/java/com/sonar/AppTest.java
@@ -0,0 +1,12 @@
+package com.sonar;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void test() {
+        System.out.println ("Not yet implemented");
+    }
+}
